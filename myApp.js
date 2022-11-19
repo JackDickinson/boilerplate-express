7a89a4e9762baf880464464f7b132eb2ba2dd79c
work <jack@charle.co.uk>
2022-11-19 17:42:23 +0000

    checking proccess .env variables

--- myApp.js
+++ myApp.js
@@ -1,12 +1,17 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
 app.get('/', function(req,res) {
     res.sendFile(__dirname + '/views/index.html');
 })
 
 app.get('/json', function(req,res) {
-    res.json({"message": "Hello json"})
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        res.json({"message": "HELLO JSON"});
+    } else {
+        res.json({"message": "Hello json"});
+    }
 })
 
 app.use("/public",express.static(__dirname + '/public'));
